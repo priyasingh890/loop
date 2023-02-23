f3d43507f27b7b5e61f9371785b96cb1bd2f35da
priyasingh890 <110822414+priyasingh890@users.noreply.github.com>
2023-02-23 09:23:46 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,172 @@
+
+
+
+# i=1
+# while i<5:
+#     b=1
+#     while b<5-i:
+#         print("",end=" ")
+#         b=b+1
+#     j=i
+#     while j>=1:
+#         print(j,end="")
+#         j=j-1
+#     j=2
+#     while j<=i:
+#         print(j,end="")
+#         j=j+1
+#     print()
+#     i=i+1
+
+
+i=1
+while i<=6:
+    j=1
+    while j<=i:
+        print(j,end="")
+        j=j+1
+    j=i-1
+    while j>=1:
+        print(j,end="")
+        j=j-1
+    print()
+    i=i+1
+# a=int(input("enter no"))
+# i=2
+# while i<=a:
+#     j=1
+#     while j<=a:
+#         print(j*i,end=" ")
+#         j=j+1
+#     print()
+#     i=i+1
+
+
+# i=1
+# while i<=100:
+#     if i%5!=0 and i%3!=0:
+#         print(i)
+#     elif i%3==0 and i%5==0:
+#         print(i)
+#     i=i+1
+
+
+# i=1
+# while i<=100:
+#     if i%2!=0 and i%3!=0 and i%5!=0 and i!=1:
+#         print(i)
+#     elif i==2 or i==3 or i==5:  
+#         print(i)
+#     i=i+1
+# l=["110","7800", "3400"]
+# i=0
+# d=[]
+# while i<len(l):
+#     h=l[i]
+#     j=0
+#     sum=''
+#     while j<len(h):
+#         if h[j]!="0":
+#             sum=sum+h[j]
+#         j=j+1
+#     d.append(sum)
+#     i=i+1
+# print(d)
+    
+    # a=2
+    # b="2"
+    # c="3+5j"
+    # d=int(b)
+    # e=complex(c)
+    # print(a+d+e)
+
+# a={"b":4,"c":5,"x":{"l":5},"f":6}"
+# a="2.2"
+# b="pune"
+# c="2+2j"
+# d="tanu"
+# e="khose"
+# f="true123"
+# v=int(a)
+# # k=complex(c)
+# a="true"
+# b="fale"
+# print(a,b)
+# g="siya"
+# b=3+5j
+# v=7+2j
+# print(g(b+v))
+
+
+# # 
+# a=[2,7,8,[4,2,[3,7],5,],5,6]
+# i=0
+# sum=0
+# c=0
+# while i<len(a):
+#     if type(a[i])==list:
+#         j=0
+#         while j<len(a[i]):
+#             if type(a[i])==list:
+#                 print(a[i][j])
+#                 v=0
+#                 while v<len(a[i][j]):
+#                     if type(a[i][j])==list:
+                          
+
+#                 #     sum=sum+a[]
+#             else:
+#                 print(a[i][j])
+#             j=j+1
+#     else:
+#         sum=sum+(a[i])
+#     i=i+1
+        
+# i=1
+# while i>=1:
+#     j=5
+#     while j>=i:
+#         print(i)
+#         j=j-1
+#     i=i+1
+
+
+# s=int(input("enter no"))
+# i=0
+# while i<=s:
+#     b=0
+#     while b<=5-i:
+#         print(end=" ")
+#         b=b+1
+#     j=1
+#     while j<=i:
+#         print("*",end="")
+#         j=j+1
+#     print()
+#     i=i+1
+
+a= int(input("enter no."))   
+b=int(input("enter no"))
+c=int(input("enter no"))
+print(min(a,b,c))
+            
+
+            
+            
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+            
